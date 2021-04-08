815381cebfe0b5440b07032979aa10a8cfceed44
Kadir Sarıaslan <software@sariaslan.net>
2021-04-08 23:02:55 +0300

    Navi,category,product components created

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './components/category/category.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    NaviComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
